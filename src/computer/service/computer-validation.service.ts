8ab7ace2aad7381c8004d22ddbd82ef7a226a1bc
Ioannis Theodosiadis <114492036+1oannis@users.noreply.github.com>
2022-10-28 16:17:17 +0200

    removed check chars because its useless

--- src/computer/service/computer-validation.service.ts
+++ src/computer/service/computer-validation.service.ts
@@ -62,37 +62,6 @@ export class ComputerValidationService {
         return ID_PATTERN.test(id);
     }
 
-    #checkChars(chars: string[]) {
-        /* eslint-disable @typescript-eslint/no-magic-numbers, unicorn/no-for-loop, security/detect-object-injection */
-        let sum = 0;
-        let check: number | string;
-
-        if (chars.length === 9) {
-            // Compute the ISBN-10 check digit
-            chars.reverse();
-            for (let i = 0; i < chars.length; i++) {
-                sum += (i + 2) * Number.parseInt(chars[i] ?? '', 10);
-            }
-            check = 11 - (sum % 11); // eslint-disable-line @typescript-eslint/no-extra-parens
-            if (check === 10) {
-                check = 'X';
-            } else if (check === 11) {
-                check = '0';
-            }
-        } else {
-            // Compute the ISBN-13 check digit
-            for (let i = 0; i < chars.length; i++) {
-                sum += ((i % 2) * 2 + 1) * Number.parseInt(chars[i] ?? '', 10); // eslint-disable-line @typescript-eslint/no-extra-parens
-            }
-            check = 10 - (sum % 10); // eslint-disable-line @typescript-eslint/no-extra-parens
-            if (check === 10) {
-                check = '0';
-            }
-        }
-        return check;
-        /* eslint-enable @typescript-eslint/no-magic-numbers, unicorn/no-for-loop, security/detect-object-injection */
-    }
-
     // https://github.com/ajv-validator/ajv-formats/issues/14#issuecomment-826340298
     #validateSeriennummer: FormatValidator<string> = (subject: string) => {
         // Checks for serial number format
