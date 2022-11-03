eb81d106af6b0a84bd4ea917d5d9fd8a071d7a58
Jan <rija1027@h-ka.de>
2022-11-03 08:54:13 +0100

    updated getBaseUri.ts

--- src/computer/rest/getBaseUri.ts
+++ src/computer/rest/getBaseUri.ts
@@ -20,7 +20,7 @@
  * @packageDocumentation
  */
 
-import { ID_PATTERN } from '../service/computer-validation.service.js';
+import { REGEX } from '../service/computer-validation.service.js';
 import { type Request } from 'express';
 import { cloud } from '../../config/cloud.js';
 import { nodeConfig } from '../../config/node.js';
@@ -36,7 +36,7 @@ export const getBaseUri = (req: Request) => {
     const indexLastSlash = basePath.lastIndexOf('/');
     if (indexLastSlash > 0) {
         const idStr = basePath.slice(indexLastSlash + 1);
-        if (ID_PATTERN.test(idStr)) {
+        if (REGEX.test(idStr)) {
             basePath = basePath.slice(0, indexLastSlash);
         }
     }
