c1b7760ec2096dc30390f367dd0ee90b108574d3
2bagg <gutu1011@h-ka.de>
2022-11-04 17:47:02 +0100

    deleted mysql and updated postgres

--- src/config/dev/postgres/create-table.sql
+++ src/config/dev/postgres/create-table.sql
@@ -14,55 +14,40 @@
 -- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 -- docker compose exec postgres bash
--- psql --dbname=buch --username=buch --file=/scripts/create-table-buch.sql
+-- psql --dbname=computer --username=computer --file=/scripts/create-table-computer.sql
 
 -- https://www.postgresql.org/docs/devel/app-psql.html
 -- https://www.postgresql.org/docs/current/ddl-schemas.html
 -- https://www.postgresql.org/docs/current/ddl-schemas.html#DDL-SCHEMAS-CREATE
 -- "user-private schema" (Default-Schema: public)
-CREATE SCHEMA IF NOT EXISTS AUTHORIZATION buch;
+CREATE SCHEMA IF NOT EXISTS AUTHORIZATION computer;
 
-ALTER ROLE buch SET search_path = 'buch';
+ALTER ROLE computer SET search_path = 'computer';
 
 -- https://www.postgresql.org/docs/current/sql-createtable.html
 -- https://www.postgresql.org/docs/current/datatype.html
-CREATE TABLE IF NOT EXISTS buch (
+CREATE TABLE IF NOT EXISTS computer (
                   -- https://www.postgresql.org/docs/current/datatype-uuid.html
                   -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-PRIMARY-KEYS
                   -- impliziter Index fuer Primary Key
                   -- TypeORM unterstuetzt nicht BINARY(16) fuer UUID
-    id            char(36) PRIMARY KEY USING INDEX TABLESPACE buchspace,
+    id            char(36) PRIMARY KEY USING INDEX TABLESPACE computerspace,
                   -- https://www.postgresql.org/docs/current/datatype-numeric.html#DATATYPE-INT
     version       integer NOT NULL DEFAULT 0,
                   -- impliziter Index als B-Baum durch UNIQUE
                   -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-UNIQUE-CONSTRAINTS
-    titel         varchar(40) NOT NULL UNIQUE USING INDEX TABLESPACE buchspace,
+    hersteller    varchar(40) NOT NULL UNIQUE USING INDEX TABLESPACE computerspace,
                   -- https://www.postgresql.org/docs/current/ddl-constraints.html#id-1.5.4.6.6
                   -- https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-CHECK-CONSTRAINTS
-    rating        integer NOT NULL CHECK (rating >= 0 AND rating <= 5),
+    modell        varchar(12) NOT NULL CHECK (modell ~ 'Desktop-PC|Gaming-PC'),
                   -- https://www.postgresql.org/docs/current/functions-matching.html#FUNCTIONS-POSIX-REGEXP
-    art           varchar(12) NOT NULL CHECK (art ~ 'DRUCKAUSGABE|KINDLE'),
-    verlag        varchar(12) NOT NULL CHECK (verlag ~ 'FOO_VERLAG|BAR_VERLAG'),
+    herstelldatum date,
                   -- https://www.postgresql.org/docs/current/datatype-numeric.html#DATATYPE-NUMERIC-DECIMAL
                   -- 10 Stellen, davon 2 Nachkommastellen
     preis         decimal(8,2) NOT NULL,
-    rabatt        decimal(4,3) NOT NULL,
-                  -- https://www.postgresql.org/docs/current/datatype-boolean.html
-    lieferbar     boolean NOT NULL DEFAULT FALSE,
-                  -- https://www.postgresql.org/docs/current/datatype-datetime.html
-    datum         date,
-    homepage      varchar(40),
-    isbn          varchar(16) NOT NULL UNIQUE USING INDEX TABLESPACE buchspace,
+    farbe         varchar(12) NOT NULL CHECK (art ~ 'rot|schwarz'),
+    seriennummer  varchar(16) NOT NULL UNIQUE USING INDEX TABLESPACE buchspace,
                   -- https://www.postgresql.org/docs/current/datatype-datetime.html
     erzeugt       timestamp NOT NULL DEFAULT NOW(),
     aktualisiert  timestamp NOT NULL DEFAULT NOW()
-) TABLESPACE buchspace;
-
-CREATE TABLE IF NOT EXISTS schlagwort (
-    id         char(36) PRIMARY KEY USING INDEX TABLESPACE buchspace,
-    buch_id    char(36) NOT NULL REFERENCES buch,
-    schlagwort varchar(16) NOT NULL CHECK (schlagwort ~ 'JAVASCRIPT|TYPESCRIPT')
-) TABLESPACE buchspace;
-
--- default: btree
-CREATE INDEX IF NOT EXISTS schlagwort_buch_idx ON schlagwort(buch_id) TABLESPACE buchspace;
+) TABLESPACE computerspace;
