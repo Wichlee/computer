13fa3b98541bf8f62c8bc743c75937f2ffcb75b3
Erik Dwornik <dwer1011@h-ka.de>
2022-11-06 20:27:31 +0100

    removed commented out code

--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -18,7 +18,6 @@ import { HealthController } from './health.controller.js';
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
-// import { healthConfig } from '../config/health.js';
 
 @Module({
     // imports: [TerminusModule.forRoot({ errorLogStyle: 'pretty' }), HttpModule],
